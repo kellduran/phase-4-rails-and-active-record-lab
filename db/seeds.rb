23805cbb202953d3ed05c8e2544426a41eadc65a
Kelly Duran <kaduran16@gmail.com>
2022-08-28 11:44:34 -0400

    commit tree ad3a2ce9290f2a055c3f6ce5cc30410f721c682b

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name: 'Jack', last_name: 'Beanstalk', grade: 9)
+Student.create!(first_name: 'Jane', last_name: 'Sarah', grade: 9)
+Student.create!(first_name: 'Bobby', last_name: 'Bean', grade: 10)
+Student.create!(first_name: 'Bonnie', last_name: 'Btalk', grade: 10)
